93597e96072147bc5faeebb1849a53b036bcb7f8
marcelooliveira <mclricardo@gmail.com>
2018-08-11 17:24:28 -0300

    .

--- CasaDoCodigo.API/Controllers/ProdutoController.cs
+++ CasaDoCodigo.API/Controllers/ProdutoController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CasaDoCodigo.API.Queries;
+using Queries = CasaDoCodigo.API.Queries;
 using CasaDoCodigo.Models;
 using CasaDoCodigo.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CasaDoCodigo.API.Queries;
 
 namespace CasaDoCodigo.API.Controllers
 {
@@ -25,7 +26,7 @@ namespace CasaDoCodigo.API.Controllers
 
         // GET: api/Produto
         [HttpGet]
-        public async Task<IEnumerable<Produto>> Get()
+        public async Task<IEnumerable<Queries.Produto>> Get()
         {
             return await produtoQueries.GetProdutosAsync();
         }
