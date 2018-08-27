3d981f8175481ca5e6a7984b8bcd3500ae524754
marcelooliveira <mclricardo@gmail.com>
2018-08-26 22:44:15 -0300

    Projeto: CasaDoCodigo.Status

---
+++ CasaDoCodigo.Status/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CasaDoCodigo.Status.Models;
+using Microsoft.Extensions.HealthChecks;
+using CasaDoCodigo.Status.ViewModels;
+
+namespace CasaDoCodigo.Status.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHealthCheckService healthCheckService;
+
+        public HomeController(IHealthCheckService healthCheckService)
+        {
+            this.healthCheckService = healthCheckService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await healthCheckService.CheckHealthAsync();
+            var data = new HealthStatusViewModel(result.CheckStatus);
+
+            foreach (var item in data.Results)
+            {
+                data.AddResult(item.Name, item.Result);
+            }
+
+            return View(data);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
