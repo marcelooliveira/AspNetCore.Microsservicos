c932e3245c80b846b4c74f9e199f5dd9f3c93c62
marcelo oliveira <mclricardo@gmail.com>
2018-10-23 00:40:58 -0300

    .

--- CasaDoCodigo.CQRS/Controllers/AccountController.cs
+++ CasaDoCodigo.CQRS/Controllers/AccountController.cs
@@ -18,38 +18,16 @@ namespace CasaDoCodigo.Controllers
         {
             return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
         }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
     }
-
-    //[Authorize]
-    //public class AccountController : Controller
-    //{
-    //    [Authorize]
-    //    public async Task<IActionResult> SignIn(string returnUrl)
-    //    {
-    //        var user = User as ClaimsPrincipal;
-
-    //        var token = await HttpContext.GetTokenAsync("access_token");
-
-    //        if (token != null)
-    //        {
-    //            ViewData["access_token"] = token;
-    //        }
-
-    //        // "Catalog" because UrlHelper doesn't support nameof() for controllers
-    //        // https://github.com/aspnet/Mvc/issues/5853
-    //        return RedirectToAction("Carrossel", "Pedido");
-    //    }
-
-    //    public async Task<IActionResult> Signout()
-    //    {
-    //        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-    //        await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
-
-    //        // "Catalog" because UrlHelper doesn't support nameof() for controllers
-    //        // https://github.com/aspnet/Mvc/issues/5853
-    //        var homeUrl = Url.Action("Carrossel", "Pedido");
-    //        return new SignOutResult(OpenIdConnectDefaults.AuthenticationScheme,
-    //            new AuthenticationProperties { RedirectUri = homeUrl });
-    //    }
-    //}
 }
