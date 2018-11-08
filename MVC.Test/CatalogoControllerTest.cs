273c8120a5933ec4672b5414d3905132089c966a
marcelooliveira <mclricardo@gmail.com>
2018-11-08 12:41:41 -0200

    NovoCarrinhoControllerTest

--- MVC.Test/CatalogoControllerTest.cs
+++ MVC.Test/CatalogoControllerTest.cs
@@ -13,7 +13,7 @@ using Xunit;
 
 namespace MVC.Test
 {
-    public class CatalogoControllerTest
+    public class CatalogoControllerTest : BaseControllerTest
     {
         private readonly Mock<ICatalogoService> catalogoServiceMock;
         private readonly Mock<ILogger<CatalogoController>> loggerMock;
@@ -85,16 +85,6 @@ namespace MVC.Test
             //assert
             Assert.Null(model);
             Assert.True(!string.IsNullOrWhiteSpace(catalogoController.ViewBag.MsgServicoIndisponivel));
-        }
-
-        private IList<Produto> GetFakeProdutos()
-        {
-            return new List<Produto>
-            {
-                new Produto("001", "produto 001", 12.34m),
-                new Produto("002", "produto 002", 23.45m),
-                new Produto("003", "produto 003", 34.56m)
-            };
         }
     }
 }
---
+++ MVC.Test/NovoCarrinhoControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using CasaDoCodigo.Controllers;
+using CasaDoCodigo.Models.ViewModels;
+using CasaDoCodigo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MVC.Test
+{
+    public class NovoCarrinhoControllerTest : BaseControllerTest
+    {
+        private readonly Mock<IHttpContextAccessor> contextAccessor;
+        private readonly Mock<IIdentityParser<ApplicationUser>> appUserParser;
+        private readonly Mock<ILogger<CarrinhoController>> logger;
+        private readonly Mock<ICatalogoService> catalogoService;
+        private readonly Mock<ICarrinhoService> carrinhoService;
+
+        public NovoCarrinhoControllerTest()
+        {
+            contextAccessor = new Mock<IHttpContextAccessor>();
+            appUserParser = new Mock<IIdentityParser<ApplicationUser>>();
+            logger = new Mock<ILogger<CarrinhoController>>();
+            catalogoService = new Mock<ICatalogoService>();
+            carrinhoService = new Mock<ICarrinhoService>();
+        }
+
+       [Fact]
+        public async Task CarrinhoController_Index()
+        {
+            //arrange
+            var produtos = GetFakeProdutos();
+
+            //act
+            var controller = new CarrinhoController(contextAccessor.Object, appUserParser.Object, logger.Object, catalogoService.Object, carrinhoService.Object);
+            var testProduct = produtos[0];
+            var result = await controller.Index(testProduct.Codigo);
+
+            //assert
+            Assert.IsType<ViewResult>(result);
+        }       
+    }
+}
