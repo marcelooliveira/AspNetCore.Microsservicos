2be2c93d48fce0b3c0d5d3cc179d4cf27116a072
marcelooliveira <mclricardo@gmail.com>
2018-11-14 19:33:41 -0200

    .

--- Carrinho.UnitTests/RedisCarrinhoRepositoryTest.cs
+++ Carrinho.UnitTests/RedisCarrinhoRepositoryTest.cs
@@ -62,6 +62,47 @@ namespace Carrinho.API.Tests
                     Assert.Equal(7, item.Quantidade);
                 });
         }
+
+        [Fact]
+        public async Task GetCarrinhoAsync_clienteId_NotFound()
+        {
+            //arrange
+            var json = @"{
+                  ""ClienteId"": ""123"",
+                  ""Itens"": []
+                }";
+
+            string clienteId = "123";
+            var databaseMock = new Mock<IDatabase>();
+            databaseMock
+                .Setup(d => d.StringSetAsync(
+                        It.IsAny<RedisKey>(),
+                        It.IsAny<RedisValue>(),
+                        null,
+                        When.Always,
+                        CommandFlags.None
+                    ))
+               .ReturnsAsync(true);
+
+            databaseMock.SetupSequence(d => d.StringGetAsync(It.IsAny<RedisKey>(), It.IsAny<CommandFlags>()))
+                    .ReturnsAsync("")
+                    .ReturnsAsync(json);
+
+
+            redisMock
+                .Setup(r => r.GetDatabase(It.IsAny<int>(), It.IsAny<object>()))
+                .Returns(databaseMock.Object);
+
+            var repository
+                = new RedisCarrinhoRepository(loggerMock.Object, redisMock.Object);
+
+            //act
+            var carrinhoCliente = await repository.GetCarrinhoAsync(clienteId);
+
+            //assert
+            Assert.Equal(clienteId, carrinhoCliente.ClienteId);
+            Assert.Empty(carrinhoCliente.Itens);
+        }
         #endregion
 
         #region GetUsuarios
