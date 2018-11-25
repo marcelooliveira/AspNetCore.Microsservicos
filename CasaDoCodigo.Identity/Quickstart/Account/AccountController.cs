f91ea3e135a55b6de7ba9c497dd7fbd1bfc0e71e
marcelooliveira <mclricardo@gmail.com>
2018-11-25 11:38:57 -0200

    .

--- CasaDoCodigo.Identity/Quickstart/Account/AccountController.cs
+++ CasaDoCodigo.Identity/Quickstart/Account/AccountController.cs
@@ -336,7 +336,7 @@ namespace IdentityServer4.Quickstart.UI
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return Redirect("~/");
+                    return RedirectToAction("login", "account");
                 }
 
                 foreach (var error in result.Errors)
