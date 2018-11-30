90f37a26a67c95c004cb3de951ce29638e0c1bdf
marcelooliveira <mclricardo@gmail.com>
2018-11-30 00:19:11 -0200

    Historico

---
+++ CasaDoCodigo.CQRS/Controllers/PedidoController.cs
@@ -0,0 +1,22 @@
+﻿using CasaDoCodigo.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : BaseController
+    {
+        public PedidoController(ILogger<PedidoController> logger) : base(logger)
+        {
+        }
+
+        public async Task<ActionResult> Historico()
+        {
+            return View();
+        }
+    }
+}
