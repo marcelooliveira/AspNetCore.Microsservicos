88aff16bbcb10d57ec374ad4a7a1f079272018a3
marcelooliveira <mclricardo@gmail.com>
2018-12-04 15:58:38 -0200

    Notifications

--- CasaDoCodigo.CQRS/wwwroot/js/site.js
+++ CasaDoCodigo.CQRS/wwwroot/js/site.js
@@ -1,1 +1,12 @@
-﻿
+﻿var el = document.querySelector('.notification');
+
+document.querySelector('.button-notification').addEventListener('click', function () {
+    var count = Number(el.getAttribute('data-count')) || 0;
+    el.setAttribute('data-count', count + 1);
+    el.classList.remove('notify');
+    el.offsetWidth = el.offsetWidth;
+    el.classList.add('notify');
+    if (count === 0) {
+        el.classList.add('show-count');
+    }
+}, false);
