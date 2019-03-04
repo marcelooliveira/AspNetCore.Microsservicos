db5bb33a42760a2b0f19eeb792c20b4519866eb3
marcelooliveira <mclricardo@gmail.com>
2019-03-04 18:44:29 -0300

    diretórios renomeados

---
+++ MVC/Models/UserNotification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CasaDoCodigo.Models
+{
+    public class UserNotification
+    {
+        public UserNotification()
+        {
+        }
+
+        public UserNotification(string usuarioId, string mensagem, DateTime dateCreated, DateTime? dateVisualized)
+        {
+            UsuarioId = usuarioId;
+            Mensagem = mensagem;
+            DateCreated = dateCreated;
+            DateVisualized = dateVisualized;
+        }
+
+        public string UsuarioId { get; set; }
+        public string Mensagem { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateVisualized { get; set; }
+    }
+}
